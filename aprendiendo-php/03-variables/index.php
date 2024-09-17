186cc938e01c5563916a5d4206fde24065e5d705
wcarbajal <wcarbajal@outlook.com>
2024-09-17 12:24:25 -0500

    inicio

---
+++ aprendiendo-php/03-variables/index.php
@@ -0,0 +1,11 @@
+
+<?php 
+  
+  $mi_primera_variable = 'hola mundo';
+  $mi_segunda_variable = 7.25;
+  $mi_tercera_variable = true;
+  echo '<h1>'.$mi_primera_variable.'</h1>'.'<br>';
+  echo gettype($mi_segunda_variable).'<br>';
+  echo '<h1>'.$mi_tercera_variable.'</h1>'.'<br>';;
+  var_dump($mi_tercera_variable); //mostrará el tipo y valor de la variable.
+?>

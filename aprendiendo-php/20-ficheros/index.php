ac9671cd7efb2f3450f0ffff3ef7b38a62c6ffab
wuilmer carbajal <wcarbajal@outlook.com>
2024-09-18 23:27:33 -0500

    18.09

---
+++ aprendiendo-php/20-ficheros/index.php
@@ -0,0 +1,19 @@
+
+<?php 
+
+/* $archivo = fopen("fichero_texto.txt","a+");
+
+
+while(!feof($archivo)) {
+
+  $contenido = fgets($archivo);
+  echo $contenido."<br>";
+
+}
+fwrite($archivo, "Este es un nuevo dato\n");
+
+fclose($archivo); 
+copy("fichero_texto.txt", "fichero_copia.txt");*/
+
+
+unlink("fichero_renombrado.txt");
